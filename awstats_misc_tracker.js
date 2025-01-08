a9a64cb14ee71e2727b2f28b341f2ef0c9dc8ebe
Wardell Randall <135070508+smurcyjerky@users.noreply.github.com>
2025-01-08 08:09:17 -0800

    Update awstats_misc_tracker.js

--- awstats_misc_tracker.js
+++ awstats_misc_tracker.js
@@ -1,1 +1,1 @@
-<script>type="text/javascript"src="/wanug66kqfuk/local/cpanel/wwwroot/cgi-bin/awstats.pl/to/awstats/tracker.js"><script>
+<script>type="text/javascript"src="/wanug66kqfuk/local/cpanel/3rdparty/bin/awstats.pl/to/awstats/tracker.js"><script>
